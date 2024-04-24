74d3dab015447fd4ff7876aa6b65c04a4ebf7038
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-04-24 16:10:28 +0530

    add location field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('location')->nullable();
             $table->decimal('latitude', 10, 6)->default(0);
             $table->decimal('longitude', 10, 6)->default(0);
             $table->rememberToken();
