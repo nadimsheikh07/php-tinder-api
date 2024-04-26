a402d4c0a24a4cc4efb4aa8f2dc975f43e87bd4c
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-04-26 17:18:08 +0530

    add auth prefix

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::post('/signup', [UserController::class, 'signup']);
-Route::post('/signin', [UserController::class, 'signin']);
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('/signup', [UserController::class, 'signup']);
+    Route::post('/signin', [UserController::class, 'signin']);
+});
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/recommended-users', [UserController::class, 'recommendedUsers']);
